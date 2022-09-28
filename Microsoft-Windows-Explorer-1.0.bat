333cff3df190f0e62e93c96e02e8f1fcda4fdb2e
Ayman <87582743+mastergive514@users.noreply.github.com>
2022-09-28 19:30:41 +0200

    Add files via upload

---
+++ Microsoft-Windows-Explorer-1.0.bat
@@ -0,0 +1,14 @@
+echo off
+cls
+
+cd ./files
+cd ./vbsfiles
+cd ./warnings
+start warn-betterversion.vbs
+cd..
+cd..
+cd ./batfiles
+start  mwe-prt1.bat
+TITLE WINDOWS EXPLORER FOR WINDOWS 1.1
+cls
+
